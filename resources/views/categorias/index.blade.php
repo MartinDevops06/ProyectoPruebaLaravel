f2432d14b51a8b3babca468b80cf356b477e10bd
Martin Velasquez <mv.libreros06@gmail.com>
2025-11-10 14:44:34 -0500

    Creacion de Proyecto con Laravel, quede en el registro de categorias, lo que sigue a continuacion es -Validacion en el cliente-

---
+++ resources/views/categorias/index.blade.php
@@ -0,0 +1,32 @@
+@extends('layout')
+@section('title','Categorias')
+@section('content')
+
+<h3 class = "mt-4">Listado De Categorias</h3>
+<div class="text-end">
+    <a href="{{url('categorias/create')}}" class="btn btn-primary">Nuevo</a>
+</div>
+
+<table class="table">
+    <thead>
+        <th>Nombre</th>
+        <th>Descripcion</th>
+        <th>Acciones</th>
+    </thead>
+    <tbody>
+        @foreach($datos as $categoria)
+            <tr>
+                <td>
+                    {{ $categoria ->nombre}}
+                </td>
+                <td>
+                    {{ $categoria ->descripcion}}
+                </td>
+                <td>
+                    Editar - eliminar
+                </td>
+            </tr>
+        @endforeach
+    </tbody>
+</table>
+@stop()
---
+++ resources/views/index.blade.php
@@ -0,0 +1,5 @@
+@extends('layout')
+@section('title','Saludo')
+@section('content')
+    <h3>Bienvenidos a Little Wonder!!</h3>
+@stop()
